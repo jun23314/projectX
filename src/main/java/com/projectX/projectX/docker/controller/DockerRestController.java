aeb253152b15f313b6c55b2493a6dbe64b155dca
jun23314 <dlwnsfml@naver.com>
2024-03-28 16:29:55 +0900

    테스트 추가

--- src/main/java/com/projectX/projectX/docker/controller/DockerRestController.java
+++ src/main/java/com/projectX/projectX/docker/controller/DockerRestController.java
@@ -2,14 +2,16 @@ package com.projectX.projectX.docker.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerRestController {
-
     @RequestMapping("/sample")
     public String docker() {
         return "docker sample";
     }
+    @GetMapping("/")
+    public String test() {return "its test!";}
 }
